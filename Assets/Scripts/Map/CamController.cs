aa5253f9a66f173460b3f394b228e3c9f95e6075
TauMah <lenitramc@gmail.com>
2024-12-29 19:25:52 +0100

    Ajout d'un nouveau matériau "Shield" et mise à jour de la version du bundle. Modification de la valeur de zoom minimum et ajout de .vscode au .gitignore.

--- Assets/Scripts/Map/CamController.cs
+++ Assets/Scripts/Map/CamController.cs
@@ -14,7 +14,7 @@ public class CamController : MonoBehaviour
 
     // Variables pour le zoom
     private float zoomSpeed = 10f;     // Vitesse du zoom
-    private float minZoom = 10f;        // Zoom minimum (proche)
+    private float minZoom = 9f;        // Zoom minimum (proche)
     private float maxZoom = 25f;       // Zoom maximum (éloigné)
 
     void Start()
