13bd067dbba8d3c0610c1dc1d570d97757ee956f
TauMah <lenitramc@gmail.com>
2025-03-16 08:36:39 +0100

    Ajout de la fonctionnalité de mise à jour du jeu avec téléchargement, extraction et exécution du script de mise à jour

--- Assets/Scripts/LoginPage/UpdateBtn.cs
+++ Assets/Scripts/LoginPage/UpdateBtn.cs
@@ -1,108 +1,125 @@
+using System.Collections;
+using System.IO;
+using System.IO.Compression;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class UpdateBtn : MonoBehaviour
 {
-    
-    // on start, add a listener to the component button
+    private string downloadedZipPath;
+    private string extractionPath;
+    private string platform = "";
+
+    [SerializeField] private Text debugText;
+
 
     void Start()
     {
+        // Initialisation des chemins dans Start() pour éviter l'erreur
+        downloadedZipPath = Path.Combine(Application.persistentDataPath, "game_update.zip");
+        extractionPath = Path.Combine(Application.persistentDataPath, "game_update");
+
+        // Configuration de la plateforme
+#if UNITY_STANDALONE_WIN
+        platform = "windows";
+#elif UNITY_STANDALONE_LINUX
+        platform = "linux";
+#elif UNITY_STANDALONE_OSX
+        platform = "macos";
+#endif
+
+        // Ajout du listener au bouton
         GetComponent<Button>().onClick.AddListener(() => UpdateGame());
     }
 
-    // Update the map
     public void UpdateGame()
     {
-        // open the link :
-        Application.OpenURL(DataManager.Instance.GetData("serverIP") + " /");
+        // Vérifier la plateforme : Windows, MacOS, Linux
+        if (platform == "")
+        {
+            // debugTest.text("Plateforme non supportée !");
+            return;
+        }
+
+        StartCoroutine(DownloadAndUpdate());
+    }
+
+    IEnumerator DownloadAndUpdate()
+    {
+        string link = "";
+
+        if (platform == "linux")
+        {
+            link = DataManager.Instance.GetData("serverIP") + "/get_last_game_version_lin";
+        }
+        else if (platform == "macos")
+        {
+            link = DataManager.Instance.GetData("serverIP") + "/get_last_game_version_mac";
+        }
+        else if (platform == "windows")
+        {
+            link = DataManager.Instance.GetData("serverIP") + "/get_last_game_version_win";
+        }
+
+        // Commencer le téléchargement
+        UnityWebRequest www = UnityWebRequest.Get(link);
+        www.downloadHandler = new DownloadHandlerFile(downloadedZipPath);
+
+        var operation = www.SendWebRequest();
+
+        while (!operation.isDone)
+        {
+            debugText.text = $"Téléchargement en cours : {www.downloadProgress * 100}%";
+            yield return null;
+        }
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            debugText.text += ($"Erreur de téléchargement : {www.error}");
+            yield break;
+        }
+
+        // Debug.Log("Téléchargement terminé !");
+
+        // Décompresser le fichier ZIP
+        ExtractDownloadedZip();
+
+        // Exécuter le launcher externe
+        LaunchUpdaterScript();
+
+        // Fermer l’application actuelle pour permettre le remplacement
+        Application.Quit();
+    }
+
+    void ExtractDownloadedZip()
+    {
+        if (Directory.Exists(extractionPath))
+            Directory.Delete(extractionPath, true);
+
+        ZipFile.ExtractToDirectory(downloadedZipPath, extractionPath);
+        debugText.text += "Fichier ZIP extrait !";
+    }
+
+    void LaunchUpdaterScript()
+    {
+        string updaterScriptPath = Path.Combine(extractionPath, "update.bat");
+
+        if (!File.Exists(updaterScriptPath))
+        {
+            debugText.text += "Script de mise à jour introuvable !";
+            return;
+        }
+
+        ProcessStartInfo startInfo = new ProcessStartInfo()
+        {
+            FileName = updaterScriptPath,
+            WorkingDirectory = extractionPath,
+            CreateNoWindow = true,
+            UseShellExecute = false
+        };
+        Process.Start(startInfo);
+        debugText.text += "Mise à jour en cours...";
     }
 }
-
-
-
-// using UnityEngine;
-// using UnityEngine.Networking;
-// using System.Collections;
-// using System.IO;
-// using System.IO.Compression;
-// using System.Diagnostics;
-
-// public class GameUpdater : MonoBehaviour
-// {
-//     public string updateUrl = "http://VOTRE_IP:PORT/get_last_game_version";
-//     private string downloadedZipPath;
-//     private string extractionPath;
-
-//     void Start()
-//     {
-//         downloadedZipPath = Path.Combine(Application.persistentDataPath, "game_update.zip");
-//         extractionPath = Path.Combine(Application.persistentDataPath, "game_update");
-
-//         StartCoroutine(DownloadAndUpdate());
-//     }
-
-//     IEnumerator DownloadAndUpdate()
-//     {
-//         // Commencer le téléchargement
-//         UnityWebRequest www = UnityWebRequest.Get(updateUrl);
-//         www.downloadHandler = new DownloadHandlerFile(downloadedZipPath);
-
-//         var operation = www.SendWebRequest();
-
-//         while (!operation.isDone)
-//         {
-//             // Optionnel : afficher une barre de progression
-//             UnityEngine.Debug.Log($"Téléchargement en cours : {www.downloadProgress * 100}%");
-//             yield return null;
-//         }
-
-//         if (www.result != UnityWebRequest.Result.Success)
-//         {
-//             UnityEngine.Debug.LogError($"Erreur de téléchargement : {www.error}");
-//             yield break;
-//         }
-
-//         UnityEngine.Debug.Log("Téléchargement terminé !");
-
-//         // Décompresser le fichier ZIP
-//         ExtractDownloadedZip();
-
-//         // Exécuter le launcher externe
-//         LaunchUpdaterScript();
-
-//         // Fermer l’application actuelle pour permettre le remplacement
-//         Application.Quit();
-//     }
-
-//     void ExtractDownloadedZip()
-//     {
-//         if (Directory.Exists(extractionPath))
-//             Directory.Delete(extractionPath, true);
-
-//         ZipFile.ExtractToDirectory(downloadedZipPath, extractionPath);
-//         UnityEngine.Debug.Log("Extraction terminée !");
-//     }
-
-//     void LaunchUpdaterScript()
-//     {
-//         string updaterScriptPath = Path.Combine(extractionPath, "update.bat");
-
-//         if (!File.Exists(updaterScriptPath))
-//         {
-//             UnityEngine.Debug.LogError("Le script de mise à jour est introuvable !");
-//             return;
-//         }
-
-//         // Lancez le script de mise à jour
-//         ProcessStartInfo startInfo = new ProcessStartInfo()
-//         {
-//             FileName = updaterScriptPath,
-//             WorkingDirectory = extractionPath,
-//             CreateNoWindow = true,
-//             UseShellExecute = false
-//         };
-//         Process.Start(startInfo);
-//         UnityEngine.Debug.Log("Script de mise à jour lancé.");
-//     }
-// }
